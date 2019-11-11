345f21dc1e47055ad17009958e42890694a8d363
MrJellimann <46575936+MrJellimann@users.noreply.github.com>
2019-11-11 14:00:09 -0500

    Update Enemy.cs

--- Little Little Bear/Assets/Scripts/Enemy.cs
+++ Little Little Bear/Assets/Scripts/Enemy.cs
@@ -5,10 +5,119 @@ using UnityEngine;
 public class Enemy : BasicEntity
 {
 	//===== Parameters =====//
+	ArrayList visitedPoints;
+	//Vector2 visited;
 
 	//===== Stats =====//
 
 	//===== Methods =====//
+	public Enemy()
+	{
+		visitedPoints = new ArrayList();
+	}
+
+	public bool lineOfSight(BasicEntity entity, GridCell[,] map)
+	{
+		int sx, sy, gx, gy;
+		sx = CurrentX; sy = CurrentY;
+		gx = entity.CurrentX; gy = entity.CurrentY;
+
+		int x_slope = (int) (gx - sx);
+		int y_slope = (int) (gy - sy);
+		bool flag = true;
+
+		while (flag && (sx != gx && sy != gy))
+		{
+			if (map[sx, sy].tileType == TileSet.WALL)
+				flag = false;
+
+			if (x_slope > 0)
+				sx++;
+			else
+				sx--;
+
+			if (y_slope > 0)
+				sy++;
+			else
+				sy--;
+		}
+
+		return flag;
+	}
+
+	public void pathfindTowardsEntity(BasicEntity entity, GridCell[,] map)
+	{
+		// First, update visited points
+		// Add current position to visited points
+		visitedPoints.Add(Vector2(this.CurrentX, this.CurrentY));
+
+		// Don't store more than 20 visited points
+		if (visitedPoints.Count > 20)
+			visitedPoints.RemoveAt(0); // Remove oldest point
+
+		// Now, check distance from each position adjacent to entity
+		int[4] dist;
+		dist[0] = Mathf.sqrt((entity.CurrentX - this.CurrentX)*(entity.CurrentX - this.CurrentX) + (entity.CurrentY - this.CurrentY - 1)*(entity.CurrentY - this.CurrentY - 1));
+		dist[1] = Mathf.sqrt((entity.CurrentX - this.CurrentX)*(entity.CurrentX - this.CurrentX) + (entity.CurrentY - this.CurrentY + 1)*(entity.CurrentY - this.CurrentY + 1));
+		dist[2] = Mathf.sqrt((entity.CurrentX - this.CurrentX + 1)*(entity.CurrentX - this.CurrentX + 1) + (entity.CurrentY - this.CurrentY)*(entity.CurrentY - this.CurrentY));
+		dist[3] = Mathf.sqrt((entity.CurrentX - this.CurrentX - 1)*(entity.CurrentX - this.CurrentX - 1) + (entity.CurrentY - this.CurrentY)*(entity.CurrentY - this.CurrentY));
+
+		// Check for walls
+		if (map[this.CurrentX, this.CurrentY-1].tileType == TileSet.WALL)
+			dist[0] = 9999;
+		if (map[this.CurrentX, this.CurrentY+1].tileType == TileSet.WALL)
+			dist[1] = 9999;
+		if (map[this.CurrentX+1, this.CurrentY].tileType == TileSet.WALL)
+			dist[2] = 9999;
+		if (map[this.CurrentX-1, this.CurrentY].tileType == TileSet.WALL)
+			dist[3] = 9999;
+
+		// Check that we haven't visited these positions
+		if (visitedPoints.Contains(Vector2(this.CurrentX, this.CurrentY-1)))
+			dist[0] = 9999;
+		if (visitedPoints.Contains(Vector2(this.CurrentX, this.CurrentY+1)))
+			dist[1] = 9999;
+		if (visitedPoints.Contains(Vector2(this.CurrentX+1, this.CurrentY)))
+			dist[2] = 9999;
+		if (visitedPoints.Contains(Vector2(this.CurrentX-1, this.CurrentY)))
+			dist[3] = 9999;
+
+		// Store variables to reference later
+		int northDist, southDist, eastDist, westDist;
+		northDist = dist[0];
+		southDist = dist[1];
+		eastDist = dist[2];
+		westDist = dist[3];
+
+		// Sort to be the lowest
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = i; j < 4; j++)
+			{
+				if (dist[j] < dist[i])
+				{
+					int temp = dist[j];
+					dist[j] = dist[i];
+					dist[i] = temp;
+				}
+			}
+		}
+
+		// Actually move based on our findings
+		if (dist[0] == northDist)
+			CurrentY -= 1;
+		else if (dist[0] == southDist)
+			CurrentY += 1;
+		else if (dist[0] == eastDist)
+			CurrentX += 1;
+		else if (dist[0] == westDist)
+			CurrentX -= 1;
+		else
+		{
+			Debug.Log("Error: enemy attempted to pathfind with non-existant distance");
+		}
+	}
+
 	public void moveTowardsEntity(BasicEntity entity)
 	{
 		// If the entity we're moving towards exists
