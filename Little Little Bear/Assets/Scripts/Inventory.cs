ae0c0e9e1f743998853a14743e9c73ea0a23e856
Thomas Harrod <tharrod1@gmail.com>
2019-11-11 22:39:45 -0500

    SaveData stuff

---
+++ Little Little Bear/Assets/Scripts/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Sprites;
+public class Inventory : MonoBehaviour
+{
+    public Sprite[] spritesForShit = new Sprite[10];
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = spritesForShit[Random.Range(0,10)];
+    }
+
+
+}
