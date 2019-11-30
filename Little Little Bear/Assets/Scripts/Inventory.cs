abfaf198d34775c08bc7ef99f69c4cb50980896e
Joseph Potter <47720845+JosephPotter97@users.noreply.github.com>
2019-11-29 19:12:53 -0500

    Revert "Revert "Merge branch 'master' into JoeyBranch""
    This reverts commit f77a06ad99a2b724472e7e4b6bb8f6696c28d290.

--- Little Little Bear/Assets/Scripts/Inventory.cs
+++ Little Little Bear/Assets/Scripts/Inventory.cs
@@ -1,7 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/*
+using UnityEngine.Sprites;
+public class Inventory : MonoBehaviour
+{
+    public Sprite[] spritesForShit = new Sprite[10];
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = spritesForShit[Random.Range(0,10)];
+    }
 
+
+
+*/
 public class Inventory : MonoBehaviour
 {
     public bool[] isFull;
@@ -17,4 +30,5 @@ public class Inventory : MonoBehaviour
     {
 
     }
+
 }
