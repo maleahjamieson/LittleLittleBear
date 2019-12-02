9d624d983edc9e057382d0a3f90cccc5b2bef938
DCunoUCF <cunodavida@gmail.com>
2019-12-02 01:15:25 -0500

    Music, Ambience. WIP

---
+++ Little Little Bear/Assets/Scripts/Ambience.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ambience : MonoBehaviour
+{
+    public AudioClip SoundClip;
+    public AudioSource SoundSource;
+    public int depth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundSource.clip = SoundClip;
+        SoundSource.volume = 0.1f;
+        depth = GetComponent<LLB>().DungeonDepth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(depth < 4)
+        {
+            SoundClip = Resources.Load<AudioClip>("Sounds/forest");
+            SoundSource.Play();
+        }
+        else if(depth < 7)
+        {
+            SoundClip = Resources.Load<AudioClip>("Sounds/pond");
+            SoundSource.Play();
+        }
+        else
+        {
+            SoundClip = Resources.Load<AudioClip>("Sounds/cave");
+            SoundSource.Play();
+        }
+    }
+}
---
+++ Little Little Bear/Assets/Scripts/Music.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+    public int depth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicSource.clip = MusicClip;
+        depth = GetComponent<LLB>().DungeonDepth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(depth < 4)
+        {
+            //MusicSource = Resources.Load<AudioSource>("/Sounds/forest1");
+            //MusicSource.Play();
+        }
+    }
+}
